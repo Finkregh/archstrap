5f31716cdeec7cdefabdbdea3a6eadd310ebcf87
David Arnold <darnold@b1-systems.de>
2020-04-02 08:46:30 +0200

    Just started

---
+++ step1.sh
@@ -0,0 +1,42 @@
+!/bin/bash
+
+## create UEFI partition
+cgdisk /dev/vda
+mkfs.vfat -F32 -n EFI /dev/vda1
+
+#cryptsetup
+cryptsetup --use-random luksFormat /dev/vda2
+cryptsetup luksOpen /dev/vda2 luks
+pvcreate /dev/mapper/luks
+vgcreate vg0 /dev/mapper/luks
+lvcreate --size 4G vg0 --name swap
+lvcreate -l +100%FREE vg0 --name root
+mkfs.ext4 -L root /dev/mapper/vg0-root
+mkswap /dev/mapper/vg0-swap
+mount /dev/mapper/vg0-root /mnt
+swapon /dev/mapper/vg0-swap
+
+mkdir /mnt/boot
+mount /dev/vda1 /mnt/boot
+pacstrap /mnt linux linux-firmware base base-devel efibootmgr dialog intel-ucode lvm2 dhcpcd netctl vim ansible zsh git sudo
+while true; do
+	read -p 'Do you want to install software for notebooks (e.g. wifi): y/n ' yn
+	case $yn in
+		[Yy]* ) pacstrap /mnt wpa_supplicant;;
+		[Nn]* ) break;;
+                * ) echo "Please choose yes or no";;
+	esac
+done
+
+while true; do
+	read -p 'Do you want to install i3 Destop environment?: y/n ' yn
+	case $yn in
+		[Yy]* ) pacstrap /mnt i3-vm xorg xorg-xinit i3blocks i3lock i3status;;
+		[Nn]* ) break;;
+	esac
+done
+
+genfstab -pU /mnt | tee -a /mnt/etc/fstab
+cp ./step2.sh /mnt/root/step2.sh
+chmod +x /mnt/root/step2.sh
+arch-chroot /mnt /root/step2.sh
---
+++ step2.sh
@@ -0,0 +1,46 @@
+ln -s /usr/share/zoneinfo/Europe/Berlin /etc/localtime
+hwclock --systohc --utc
+
+#Set Hostname
+read -p "Please enter the hostname: " hostn
+echo $hostn > /etc/hostname
+echo "127.0.0.1	   "$hostn".local   "$hostn >> /etc/hosts
+
+echo LANG=en_US.UTF-8 >> /etc/locale.conf
+echo LC_ALL= >> /etc/locale.conf
+
+echo "de_DE.UTF-8 UTF-8" > /etc/locale.gen
+echo "de_DE ISO-8859-1" >> /etc/locale.gen
+echo "de_DE@euro ISO-8859-15"  >> /etc/locale.gen
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+echo "en_US ISO-8859-1" >> /etc/locale.gen
+
+locale-gen
+
+echo KEYMAP=de-latin1 >> /etc/vconsole.conf
+echo FONT=lat9w-16 >> /etc/vconsole.conf
+echo FONT_MAP=8859-1_to_uni >> /etc/vconsole.conf
+
+sed -i '/MODULES=()/c\MODULES=(ext4)' /etc/mkinitcpio.conf
+sed -i '/HOOKS=(base udev autodetect modconf block filesystems keyboard fsck)/c\HOOKS=(base udev autodetect modconf block keyboard keymap encrypt lvm2 filesystems resume fsck shutdown)' /etc/mkinitcpio.conf
+
+mkinitcpio -p linux
+bootctl --path=/boot install
+
+echo "Please set root password"
+passwd
+
+echo default arch > /boot/loader/loader.conf
+echo timeout 5 >> /boot/loader/loader.conf
+
+UUID=`blkid | grep vda2 | awk -F "\"" '{ print $2 }'`
+##cryptsetup
+echo "title Arch Linux
+linux /vmlinuz-linux
+initrd /intel-ucode.img
+initrd /initramfs-linux.img
+options cryptdevice=UUID=$UUID:vg0 root=/dev/mapper/vg0-root resume=/dev/mapper/vg0-swap rw intel_pstate=no_hwp" >> /boot/loader/entries/arch.conf
+
+systemctl enable dhcpcd.service
+
+exit
